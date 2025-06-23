76341212926022749d10c1ac2f26f4dd21c6cb01
psrtech <karthickprabakaran06@gmail.com>
2025-06-23 11:04:35 +0530

    Fix Module type

--- Frontend/postcss.config.js
+++ Frontend/postcss.config.js
@@ -1,7 +1,6 @@
-import tailwindcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
-
-/** @type {import('postcss').Config} */
-export default {
-  plugins: [tailwindcss, autoprefixer],
+module.exports = {
+  plugins: [
+    require('@tailwindcss/postcss'),
+    require('autoprefixer'),
+  ],
 };
